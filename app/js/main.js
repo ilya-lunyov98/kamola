49358842b1ca0f79a932827641e3ef86f475f74e
ilya-lunyov98 <ilya.lunyov.98@gmail.com>
2020-04-13 16:53:01 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,10 @@ $(function(){
         $(this).children('.price-item__price').toggleClass('orange')
     })
 
-    
+    $('.customers-slider__items').slick({
+        arrows:false,
+        dots: true
+      });
 
     var mixer = mixitup('.latest-project__elements');
 });
