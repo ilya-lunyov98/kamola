72545366fbe72574f47115217918de49014e0d4f
ilya-lunyov98 <ilya.lunyov.98@gmail.com>
2020-04-14 16:26:53 +0300

    adaptive menu

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,22 @@ $(function(){
         dots: true
       });
 
+    $('.menu__burger').click(function(){
+        $('.menu__line').toggleClass('active'),
+        $('.menu__list').toggleClass('slide')
+    });
+
+    window.onscroll = function showMenu()
+    {
+        var menu = document.querySelector('.menu__burger');
+
+        if(window.pageYOffset > 1280){
+            menu.classList.add('menu__fixed');
+        } else{
+            menu.classList.remove('menu__fixed');
+        }
+    }
+
     var mixer = mixitup('.latest-project__elements');
 });
 
